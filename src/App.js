26474f51df8bca852f5e7dbb1e43f97fc280d8ee
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-11-19 19:25:43 +0400

    feat: Сделал добавление задач. Добавил catch для оповещений об ошибках

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { List, AddList, Tasks } from './components';
 function App() {
   const [lists, setLists] = useState(null);
   const [colors, setColors] = useState(null);
+  const [activeItem, setActiveItem] = useState(null);
 
   useEffect(() => {
     axios.get('http://localhost:3001/lists?_expand=color&_embed=tasks')
@@ -24,12 +25,33 @@ function App() {
     setLists(newList);
   };
   
+  const onAddTask = (listId, taskObj) => {
+    const newList = lists.map(item => {
+      if (item.id === listId) {
+        item.tasks = [...item.tasks, taskObj];
+      }
+      return item;
+    });
+    setLists(newList);
+  };
+
+  const onEditListTitle = (id, title) => {
+    const newList = lists.map(item => {
+      if (item.id === id) {
+        item.name = title;
+      }
+      return item;
+    });
+    setLists(newList);
+  };
+  
   return (
     <div className="todo">
       <div className="todo__sidebar">
         <List
           items={[
             {
+              active: true,
               icon: (
                 <svg
                   width="18"
@@ -55,6 +77,10 @@ function App() {
               const newLists = lists.filter(item => item.id !== id);
               setLists(newLists);
             }}
+            onClickItem={item => {
+              setActiveItem(item);
+            }}
+            activeItem={activeItem}
             isRemovable
           />
         ) : (
@@ -62,7 +88,15 @@ function App() {
         )}
         <AddList onAdd={onAddList} colors={colors} />
       </div>
-      <div className="todo__tasks">{lists && <Tasks list={lists[1]} />}</div>
+      <div className="todo__tasks">
+        {lists && activeItem && (
+          <Tasks
+            list={activeItem}
+            onAddTask={onAddTask}
+            onEditTitle={onEditListTitle}
+          />
+        )}
+      </div>
     </div>
   );
 }
